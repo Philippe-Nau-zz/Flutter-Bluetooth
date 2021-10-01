9cb560e9d5b2281c739c73b9723d31898caf1bde
Philippe-Nau <philippe.bnu@gmail.com>
2021-10-01 10:30:46 -0300

    [ADD] added back screen when divice disconnect

--- lib/presentation/device/view/device_view.dart
+++ lib/presentation/device/view/device_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_blue/flutter_blue.dart';
 import 'package:flutter_bluetooth/presentation/device/widgets/disconnect_button.dart';
+import 'package:flutter_bluetooth/presentation/home/widgets/device_loader.dart';
 import 'package:flutter_bluetooth/widgets/app_bar.dart';
 import 'package:flutter_bluetooth/widgets/main_container.dart';
 
@@ -9,34 +10,41 @@ class DeviceView extends StatelessWidget {
   const DeviceView({
     required this.device,
   });
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Theme.of(context).backgroundColor,
-      body: CustomScrollView(
-        slivers: [
-          MyAppBar(
-            title: device.name,
-          ),
-          SliverFillRemaining(
-            hasScrollBody: false,
-            child: MainContainer(
-              pdBottom: 20,
-              child: Column(
-                children: [
-                  DisconnectButton(
-                    onTap: () async {
-                      await device.disconnect();
-                      Navigator.of(context).pop();
-                    },
+    return StreamBuilder<BluetoothDeviceState>(
+        stream: device.state,
+        initialData: BluetoothDeviceState.connected,
+        builder: (context, snapshot) {
+          if (snapshot.data == BluetoothDeviceState.disconnected) {
+            Navigator.of(context).pop();
+          }
+          return Scaffold(
+            backgroundColor: Theme.of(context).backgroundColor,
+            body: CustomScrollView(
+              slivers: [
+                MyAppBar(
+                  title: device.name,
+                ),
+                SliverFillRemaining(
+                  hasScrollBody: false,
+                  child: MainContainer(
+                    pdBottom: 20,
+                    child: Column(
+                      children: [
+                      
+                        DisconnectButton(
+                          onTap: () async {
+                            await device.disconnect();
+                          },
+                        ),
+                      ],
+                    ),
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
-          ),
-        ],
-      ),
-    );
+          );
+        });
   }
 }
